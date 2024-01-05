410c672f1650d52f065d85996111123b5e9566f2
im-a-robo <nmdas05x@gmail.com>
2024-01-04 19:19:10 -0500

    added ability to use serial coms

--- include/control/ControlNode_node.hpp
+++ include/control/ControlNode_node.hpp
@@ -2,18 +2,23 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include "serial/serial.h"
 
 class ControlNode : public rclcpp::Node {
 private:
 
-    /// Subscriber for receiving joystick messages
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joystick_sub;
+    serial::Serial my_serial = serial::Serial("/dev/ttyACM0", 9600, serial::Timeout::simpleTimeout(3000));
 
+    // Rate of rotation
     float spin_rate = 1.0f;
 
-    float base_plate_angle = 0.0f;
-    float mid_arm_angle = 0.0f;
-    float top_arm_angle = 0.0f;
+    // Angles to be published to the servos
+    float base_plate_angle = 90.0f;
+    float mid_arm_angle = 90.0f;
+    float top_arm_angle = 90.0f;
+
+    /// Subscriber for receiving joystick messages
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joystick_sub;
 
 public:
     ControlNode(const rclcpp::NodeOptions& options);
--- src/ControlNode_node.cpp
+++ src/ControlNode_node.cpp
@@ -4,7 +4,6 @@
 using namespace std::placeholders;
 
 ControlNode::ControlNode(const rclcpp::NodeOptions& options) : Node("ControlNode", options) {
-
     // Pub Sub
     this->joystick_sub =
         this->create_subscription<sensor_msgs::msg::Joy>("/joy", 1, std::bind(&ControlNode::joystick_cb, this, _1));
@@ -14,10 +13,27 @@ ControlNode::ControlNode(const rclcpp::NodeOptions& options) : Node("ControlNode
 }
 
 void ControlNode::joystick_cb(const sensor_msgs::msg::Joy::SharedPtr msg) {
-
     // right stick left and right move the base plate
+    base_plate_angle += msg->axes[3] * spin_rate;
 
     // right stick up and down move mid arm up and down
+    mid_arm_angle += msg->axes[4] * spin_rate;
 
     // left stick up and down move top arm up and down
+    top_arm_angle += msg->axes[1] * spin_rate;
+
+    std::string serial_msg = "%.2g %.2g %.2g\n", base_plate_angle, mid_arm_angle, top_arm_angle;
+
+    if (my_serial.isOpen()) {
+        std::cout << "Port opened succesfully" << std::endl;
+    } else {
+        std::cout << "Port failed to open" << std::endl;
+    }
+    my_serial.flushOutput();
+
+    my_serial.write(serial_msg);
+
+    my_serial.flushOutput();
+
+    // RCLCPP_INFO(this->get_logger(), "A %.4g B %.4g C %.4g", base_plate_angle, mid_arm_angle, top_arm_angle);
 }
